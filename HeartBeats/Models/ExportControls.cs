412f21bdd4e0dbb11c93e34c8a51889f3038786f
Roshan.Lal <Roshan.Lal@ust.com>
2024-07-05 13:57:42 +0530

    Change for setting the Root directory as the root directory available other than C if any if the path is relative

--- HeartBeats/Models/ExportControls.cs
+++ HeartBeats/Models/ExportControls.cs
@@ -1,7 +1,28 @@
-﻿namespace HeartBeats.Models
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace HeartBeats.Models
 {
     public class ExportControls
     {
-        public string ExportFilePath { get; set; } = "/Report.docx";
+        public string ExportFilePath { get; set; } = @"\Wolseley\HeartbeatReport.docx";
+
+        public ExportControls()
+        {
+            var availableDrives = GetAvailableDrivesExcludingC();
+            if(availableDrives.Length > 0)
+            {
+                ExportFilePath = availableDrives[0]+ ExportFilePath.Trim('\\');
+            }
+        }
+
+        private string[] GetAvailableDrivesExcludingC()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            return Array.FindAll(allDrives, drive => drive.IsReady && drive.DriveType == DriveType.Fixed && drive.Name != "C:\\")
+                        .Select(drive => drive.Name)
+                        .ToArray();
+        }
     }
 }
